02099198e2ad737dc460ea539983cafc4720874a
Gabor Hornyak <gabor_hornyak@epam.com>
2026-02-24 09:15:14 +0100

    fix: use named export for 'percyScreenshot'
    You can't have multiple default export in a module.

--- types/index.test-d.ts
+++ types/index.test-d.ts
@@ -1,22 +1,34 @@
 import { expectType, expectError } from 'tsd';
 import * as Playwright from 'playwright';
 import percySnapshot from '.';
-import percyScreenshot from '.';
+import { percyScreenshot } from '.';
 
 declare const page: Playwright.Page;
 
+//@ts-expect-error
 expectError(percySnapshot());
+//@ts-expect-error
 expectError(percySnapshot(page));
+//@ts-expect-error
 expectError(percySnapshot('Snapshot name'));
+//@ts-expect-error
 expectError(percyScreenshot());
+//@ts-expect-error
 expectError(percyScreenshot(page));
+//@ts-expect-error
 expectError(percyScreenshot('Snapshot name'));
 
 expectType<Promise<void>>(percySnapshot(page, 'Snapshot name'));
-expectType<Promise<void>>(percySnapshot(page, 'Snapshot name', { widths: [1000] }));
+expectType<Promise<void>>(
+  percySnapshot(page, 'Snapshot name', { widths: [1000] }),
+);
 
 expectType<Promise<void>>(percyScreenshot(page, 'Snapshot name'));
-expectType<Promise<void>>(percyScreenshot(page, 'Snapshot name', { widths: [1000] }));
+expectType<Promise<void>>(
+  percyScreenshot(page, 'Snapshot name', { widths: [1000] }),
+);
 
+//@ts-expect-error
 expectError(percySnapshot(page, 'Snapshot name', { foo: 'bar' }));
+//@ts-expect-error
 expectError(percyScreenshot(page, 'Snapshot name', { foo: 'bar' }));
